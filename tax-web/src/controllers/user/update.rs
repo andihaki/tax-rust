9c471af8fa130d807cac71a18eee3bdd7a8a1372
andihaki <andi.hakim.arif@gmail.com>
2026-01-11 17:57:48 +0700

    feat: patch user

--- tax-web/src/controllers/user/update.rs
+++ tax-web/src/controllers/user/update.rs
@@ -1,24 +1,47 @@
 use std::sync::{Arc, Mutex};
 
 use actix_web::{HttpResponse, Responder, patch, web};
+use serde::Deserialize;
 use sqlite::Connection;
 
-// @todo: send it using request body?
-#[patch("/user/{id}/{firstName}/{lastName}/{age}")]
+#[derive(Deserialize, Debug)]
+struct User {
+    first_name: Option<String>,
+    last_name: Option<String>,
+    age: Option<i32>,
+}
+
+#[patch("/user/{id}")]
 async fn handler(
-    path: web::Path<(i32, String, String, i32)>,
+    path: web::Path<i32>,
+    user: web::Json<User>,
     db: web::Data<Arc<Mutex<Connection>>>,
 ) -> impl Responder {
-    let (id, first_name, last_name, age) = path.into_inner();
+    let id = path.into_inner();
+    // println!("{:?}", user);
 
+    let first_name = match &user.first_name {
+        Some(value) => format!("firstName = '{}',", value),
+        None => format!(""),
+    };
+    let last_name = match &user.last_name {
+        Some(value) => format!("lastName = '{}',", value),
+        None => format!(""),
+    };
+    let age = match user.age {
+        Some(age) => format!("age = {}", age),
+        None => format!(""),
+    };
     let query = format!(
-        "
-        UPDATE users 
-        SET firstName = {}, lastName = {}, age = {} 
-        where id = {}",
+        "UPDATE users 
+         SET {} {} {} 
+         WHERE id = {}",
         first_name, last_name, age, id
     );
-    let db = db.as_ref().lock().unwrap();
+
+    // println!("{}", query);
+
+    let db = db.lock().unwrap();
     let result = db.execute(query);
 
     match result {
